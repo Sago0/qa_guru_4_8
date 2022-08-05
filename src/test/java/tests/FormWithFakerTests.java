720bb238287be519820945981fcec763ede78ada
Sago0 <sergey.gapeev@okay.qa>
2022-08-05 18:57:52 +0300

    added AttachmentsHelper and update test

--- src/test/java/tests/FormWithFakerTests.java
+++ src/test/java/tests/FormWithFakerTests.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 
 public class FormWithFakerTests extends TestBase {
@@ -33,56 +34,58 @@ public class FormWithFakerTests extends TestBase {
                 state = "Uttar Pradesh",
                 city = "Merrut";
 
-        open("https://demoqa.com/automation-practice-form");
-
-//      set main
-        $("#firstName").val(firstName);
-        $("#lastName").val(lastName);
-        $("#userEmail").val(email);
-        $("#genterWrapper").$(byText(gender)).click();
-        $("#userNumber").val(mobile);
-
-//      set date
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption(monthOfBirth);
-        $(".react-datepicker__year-select").selectOption(yearOfBirth);
-        $(".react-datepicker__day--0" + dayOfBirth).click();
-
-//      set subject
-        $("#subjectsInput").val(subject1);
-        $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
-        $("#subjectsInput").val(subject2);
-        $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
-
-//      set hobbies
-        $("#hobbiesWrapper").$(byText(hobby1)).click();
-        $("#hobbiesWrapper").$(byText(hobby2)).click();
-        $("#hobbiesWrapper").$(byText(hobby3)).click();
-
-//      upload file
-        $("#uploadPicture").uploadFromClasspath(picture);
-
-//      set address
-        $("#currentAddress").val(currentAddress);
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText(state)).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText(city)).click();
-
-        $("#submit").click();
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-//      asserts
-        $(".modal-content").shouldHave(text(firstName + " " + lastName),
-                text(email),
-                text(gender),
-                text(mobile),
-                text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth),
-                text(currentAddress),
-                text(picture),
-                text(subject1 + ", " + subject2),
-                text(hobby1 + ", " + hobby2 + ", " + hobby3),
-                text(state + " " + city));
-
+        step("Open students registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
+        });
+        step("Set main params", () -> {
+            $("#firstName").val(firstName);
+            $("#lastName").val(lastName);
+            $("#userEmail").val(email);
+            $("#genterWrapper").$(byText(gender)).click();
+            $("#userNumber").val(mobile);
+        });
+        step("Set date params", () -> {
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption(monthOfBirth);
+            $(".react-datepicker__year-select").selectOption(yearOfBirth);
+            $(".react-datepicker__day--0" + dayOfBirth).click();
+        });
+        step("Set subject", () -> {
+            $("#subjectsInput").val(subject1);
+            $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+            $("#subjectsInput").val(subject2);
+            $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+        });
+        step("Set hobbies", () -> {
+            $("#hobbiesWrapper").$(byText(hobby1)).click();
+            $("#hobbiesWrapper").$(byText(hobby2)).click();
+            $("#hobbiesWrapper").$(byText(hobby3)).click();
+        });
+        step("Upload file", () -> {
+            $("#uploadPicture").uploadFromClasspath(picture);
+        });
+        step("Set address", () -> {
+            $("#currentAddress").val(currentAddress);
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText(state)).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText(city)).click();
+        });
+        step("Submit", () -> {
+            $("#submit").click();
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        });
+        step("Verify successful form submit", () -> {
+            $(".modal-content").shouldHave(text(firstName + " " + lastName),
+                    text(email),
+                    text(gender),
+                    text(mobile),
+                    text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth),
+                    text(currentAddress),
+                    text(picture),
+                    text(subject1 + ", " + subject2),
+                    text(hobby1 + ", " + hobby2 + ", " + hobby3),
+                    text(state + " " + city));
+        });
     }
 }
